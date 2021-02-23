fc8e28f1bb6516b3b37d63ed33435ddead66c114
dam <dam@10.100.100.34>
2021-02-23 13:15:44 +0100

    Push 2

---
+++ src/refacforizar/Clase2.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package refacforizar;
+
+/**
+ *
+ * @author dam
+ */
+public class Clase2 {
+    //Aqui van otras lineas de codigo.
+    //Espero les guste y den like al video.
+}
